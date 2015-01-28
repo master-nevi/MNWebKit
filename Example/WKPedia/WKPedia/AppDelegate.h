6cc02fe94796a80280d0baba03da131f03e13c29
David Robles <master-nevi@users.noreply.github.com>
2015-01-27 21:44:59 -0800

    Add empty example project.

---
+++ Example/WKPedia/WKPedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKPedia
+//
+//  Created by David Robles on 1/27/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/WKPedia/WKPedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKPedia
+//
+//  Created by David Robles on 1/27/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
